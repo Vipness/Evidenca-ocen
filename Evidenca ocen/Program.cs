01550870e3836c79a3e3db5c630236a05283a300
Vipness <vid.kovacic2005@gmail.com>
2023-01-03 22:41:16 +0100

    Add WORKING dodaj method Checks for correct input of subject. Finds the row and column index of the first free space.

--- Evidenca ocen/Program.cs
+++ Evidenca ocen/Program.cs
@@ -15,24 +15,24 @@ for (int i = 0; i < st_pred; i++){
     ocene[0, i + 1] = Console.ReadLine();
 }
 
+izpis(ocene);
+
 string[] ne = { "ne", "n", "no" };
 string input = "ja";
 
 do
 {
-    izberi(ocene);
-
-    Console.Write("Ali želiš nadaljevati? ");
+    ocene = izberi(ocene);
+    izpis(ocene);
+    Console.WriteLine("\nAli želiš nadaljevati? (za zaustavitev vnesi 'ne', 'n' ali 'no') ");
     input = Console.ReadLine().ToLower();
 }
 while (!(ne.Contains(input)));
 
-static void izberi(string[,] ocene)
+static string[,] izberi(string[,] ocene)
 {
-    Console.Clear();
-
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Izberi kaj želiš narediti:");
+    Console.WriteLine("\nIzberi kaj želiš narediti:");
     Console.ResetColor();
 
     Console.WriteLine("1 - Dodaj oceno \n2 - Uredi oceno \n3 - Izbriši predmet \n4 - Izpiši ocene za določen predmet");
@@ -41,8 +41,8 @@ static void izberi(string[,] ocene)
 
     switch (st_izbire)
     {
+        case 1: ocene = dodaj(ocene); break;
         /*
-        case 1: ocene = dodaj(ocene); break; // probi kaj bi se zgodilo če se ne zapiše v ocene
         case 2: ocene = uredi(ocene); break;
         case 3: ocene = izbriši(ocene); break;
         case 4: izpiši_za_predmet(ocene); break;
@@ -54,7 +54,57 @@ static void izberi(string[,] ocene)
             izberi(ocene);
             break;
     }
-    izpis(ocene);
+
+    return ocene;
+}
+
+static string[,] dodaj(string[,] ocene)
+{
+    Console.Write("\nPri katerem predmetu si želiš dodati oceno? ");
+    string predmet = Console.ReadLine();
+
+    int stolp_predmeta = najdi_predmet(ocene, predmet);
+    if (stolp_predmeta == -1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Predmet ni bil najden! Poskusi znova.");
+        Console.ResetColor();
+        dodaj(ocene);
+    }
+
+    int vrsta = 0;
+    while (!(String.IsNullOrWhiteSpace(ocene[vrsta, stolp_predmeta])))
+    {
+        vrsta++;
+        if (vrsta >= ocene.GetLength(0)) // če je out of range
+            ocene = povecaj(ocene);
+    }
+
+    Console.Write("Vnesi pridobljeno oceno: ");
+    ocene[vrsta, stolp_predmeta] = Console.ReadLine();
+
+    return ocene;
+}
+
+static string[,] povecaj(string[,] ocene)
+{
+    string[,] nova = new string[ocene.GetLength(0)+1, ocene.GetLength(1)];
+
+    for(int i = 0; i < ocene.GetLength(0); i++)
+        for(int j = 0; j < ocene.GetLength(1); j++)
+            nova[i, j] = ocene[i, j];
+
+    return nova;
+}
+
+static int najdi_predmet(string[,] ocene, string predmet)
+{
+    for(int j = 1; j < ocene.GetLength(1); j++)
+    {
+        if (ocene[0, j] == predmet)
+            return j;
+    }
+    return -1;
 }
 
 static void izpis(string[,] t)
