a44223750277b96bf08b468aa312f2a7d9067fea
Vipness <vid.kovacic2005@gmail.com>
2023-01-03 21:44:25 +0100

    Add izberi method Lets you select one of the functions. Depending on your input calls a method which returns a new table. Seems to be working.

--- Evidenca ocen/Program.cs
+++ Evidenca ocen/Program.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Text;
+﻿using System.Text;
 Console.OutputEncoding = Encoding.UTF8; // izpis nastavil na utf-8, da se vidijo tudi šumniki
 Console.Title = "Evidenca ocen"; // spremenim ime konzole
 
@@ -14,18 +14,48 @@ for (int i = 0; i < st_pred; i++){
     Console.Write("Vnesi ime {0} predmeta: ", i + 1);
     ocene[0, i + 1] = Console.ReadLine();
 }
-*/
+
 string[] ne = { "ne", "n", "no" };
 string input = "ja";
 
 do
 {
-    
+    izberi(ocene);
 
     Console.Write("Ali želiš nadaljevati? ");
     input = Console.ReadLine().ToLower();
 }
 while (!(ne.Contains(input)));
+
+static void izberi(string[,] ocene)
+{
+    Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Izberi kaj želiš narediti:");
+    Console.ResetColor();
+
+    Console.WriteLine("1 - Dodaj oceno \n2 - Uredi oceno \n3 - Izbriši predmet \n4 - Izpiši ocene za določen predmet");
+
+    int st_izbire = int.Parse(Console.ReadLine());
+
+    switch (st_izbire)
+    {
+        /*
+        case 1: ocene = dodaj(ocene); break; // probi kaj bi se zgodilo če se ne zapiše v ocene
+        case 2: ocene = uredi(ocene); break;
+        case 3: ocene = izbriši(ocene); break;
+        case 4: izpiši_za_predmet(ocene); break;
+        */
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Vnesena je bila napačna številka! Poskusi ponovno."); // izpiše z rdečo
+            Console.ResetColor();
+            izberi(ocene);
+            break;
+    }
+    izpis(ocene);
+}
 
 static void izpis(string[,] t)
 {
@@ -35,10 +65,10 @@ static void izpis(string[,] t)
         for (int j = 0; j < t.GetLength(1); j++)
         {
             if(i == 0 && j > 0)
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.ForegroundColor = ConsoleColor.Yellow;
 
             else if(j == 0)
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.Write($"|\t{t[i, j]}\t|");
             Console.ResetColor();
